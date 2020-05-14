9e343ae4f7e5b2b8c09c18c7e317170a0ecfffee
Tommy Martinson <tmm2@princeton.edu>
2020-05-14 11:23:21 -0400

    add sun

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -1,16 +1,22 @@
-import { Group, AmbientLight, PointLight } from 'three';
+import { Group, AmbientLight, PointLight, MeshBasicMaterial, SphereBufferGeometry, Mesh } from 'three';
 
 class BasicLights extends Group {
     constructor(...args) {
         // Invoke parent Group() constructor with our args
         super(...args);
 
-        const ambi = new AmbientLight(0xffffff, 1.32);
-        const point = new PointLight(0xffffff, 4, 0, 2);
+        const ambi = new AmbientLight(0xffffff, 1.2);
+        const point = new PointLight(0xffffe0, 3, 0, 2);
 
         point.position.set(100,0,0);
-        
-        this.add(ambi, point);
+        var sunGeometry = new SphereBufferGeometry(6, 10, 10);
+        var sunMaterial = new MeshBasicMaterial({color:0xffffe0});
+        var sun = new Mesh(sunGeometry, sunMaterial);
+        sun.position.set(100,0,0);
+        this.mesh = sun;
+
+
+        this.add(ambi, point, sun);
     }
 }
 
